a2fd0fe7955b80c55b1ed3ff92a4d25994251338
lihaitao <lihaitao.me@qq.com>
2022-09-25 20:28:38 +0800

    first commit

---
+++ js/gitee.js
@@ -0,0 +1,5 @@
+// 获取仓库具体路径下的内容 https://gitee.com/api/v5/swagger#/getV5ReposOwnerRepoContents(Path)
+
+// 新建文件 https://gitee.com/api/v5/swagger#/postV5ReposOwnerRepoContentsPath
+
+// 更新文件 https://gitee.com/api/v5/swagger#/putV5ReposOwnerRepoContentsPath
